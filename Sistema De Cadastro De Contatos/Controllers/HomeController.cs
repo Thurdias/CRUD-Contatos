9e27cfc47021d15bcbae547d57682621b355d5db
Arthur Boucas <thurdias@gmail.com>
2023-05-21 22:37:00 -0300

    Add project files.

---
+++ Sistema De Cadastro De Contatos/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sistema_De_Cadastro_De_Contatos.Models;
+using System.Diagnostics;
+
+namespace Sistema_De_Cadastro_De_Contatos.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Sistema De Cadastro De Contatos/Repositorio/IContatoRepositorio.cs
@@ -0,0 +1,13 @@
+﻿using Sistema_De_Cadastro_De_Contatos.Models;
+
+namespace Sistema_De_Cadastro_De_Contatos.Repositorio
+{
+    public interface IContatoRepositorio
+    {
+        ContatoModel ListarPorId(int id);
+        List<ContatoModel> BuscarTodos();
+        ContatoModel Adicionar(ContatoModel contato);
+        ContatoModel Atualizar(ContatoModel contato);
+        bool Apagar(int id);
+    }
+}
